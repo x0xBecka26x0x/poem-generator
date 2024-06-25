303f04d0ba4a166e057a6986066c3061a79e82c4
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-06-24 22:14:09 -0400

    updating Js in vs code

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function poemGenerate(event) {
     let userInput = document.querySelector("#user-input");
     let apiKey = "bf8f1010b3c486eaa378at4e5eo24f84";
     let prompt = `User Input: Generate an Israeli poem about ${userInput.value}`;
-    let context = "You are a peaceful poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user input. Sign the poem at the end with <strong>SheCodes AI</strong>";
+    let context = "You are a peaceful poem expert and love to write short poems. Your mission is to generate a 5 line poem and separate each line with a <br />. Make sure to follow the user input. Sign the poem at the end with <strong>SheCodes AI</strong>";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
     let poemElement = document.querySelector("#poem");
